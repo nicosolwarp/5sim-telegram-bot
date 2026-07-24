bbe84a58402c2408e00d822de4b5a8c289306648
Joy Mondal <daoybarb@gmail.com>
2026-07-25 04:25:07 +0600

    Update index.js

--- index.js
+++ index.js
@@ -199,8 +199,8 @@ orders.push(order);
 
 bot.sendMessage(
     msg.chat.id,
-`
-📱 Number Purchased
+
+'📱 Number Purchased
 
 ☎️ Number:
 ${order.number}
@@ -208,8 +208,8 @@ ${order.number}
 🆔 Order ID:
 ${order.id}
 
-⏳ Waiting SMS...
-`
+⏳ Waiting SMS...'
+
 );
 
 
